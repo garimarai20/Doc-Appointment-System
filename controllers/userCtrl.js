b12df9324c7a661b1d62bd6f9bd0a2228e8162aa
garimarai20 <raigarima.id@gmail.com>
2024-08-16 12:51:20 +0530

    done

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,7 +3,13 @@ const bcrypt = require('bcryptjs')
 
 // register callback
 const registerController = async(req,res) =>{
-    
+    try{
+        
+    }catch(error){
+        console.log(error)
+        res.status(500).send({success:false,message:`Register Controller ${error.message}`})
+
+    }
 };
 
 
