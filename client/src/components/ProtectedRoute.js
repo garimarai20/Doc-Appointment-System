f37dbd5ca910369cf5aabd111dab341c92370464
garimarai20 <raigarima.id@gmail.com>
2024-09-03 14:13:17 +0530

    done

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,11 +2,11 @@ import React from 'react'
 import {Navigate} from "react-router-dom"
 
 
-export default function ProtectedRoute({children}) {
+export default function ProtectedRoute({ children }) {
   if (localStorage.getItem("token")){
-    return children 
+    return children ;
   }else{
-    return <Navigate to={'/login'} />;
+    return <Navigate to='/login' />;
   }
 
 }
--- client/src/components/PublicRoute.js
+++ client/src/components/PublicRoute.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 
-export default function PublicRoute() {
-    if (localStorage.getItem("token")){
+export default function PublicRoute({children}) {
+    if (localStorage.getItem('token')){
         return <Navigate to= '/' />;
       }else{
         return children;
