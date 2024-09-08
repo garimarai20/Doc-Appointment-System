ec97a19fc8b47ea8448708dcef8bbba90d03141f
garimarai20 <raigarima.id@gmail.com>
2024-09-08 09:26:25 +0530

    done

---
+++ client/src/components/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Layout from './Layout';
+
+const Layout = () => {
+  return (
+  <>
+  <div className='main'>
+     <div className='layout'>
+
+        <div className='sidebar'>
+
+        </div>
+
+        <div className='content'></div>
+
+     </div>
+  </div>
+  </>
+  )
+}
+
+export default Layout
